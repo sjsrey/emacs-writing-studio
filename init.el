ef164b3b50059815436607f85a9927502b6f7f6b
Serge Rey <sjsrey@gmail.com>
2025-08-06 19:45:39 -0700

    tabspaces

--- init.el
+++ init.el
@@ -1653,3 +1653,24 @@ Respond to referees:
 (use-package swiper
   :ensure t
   :bind (("C-s" . swiper)))
+
+
+;; tabspaces
+;; https://github.com/mclear-tools/tabspaces
+(use-package tabspaces
+  ;; use this next line only if you also use straight, otherwise ignore it. 
+  ;;:straight (:type git :host github :repo "mclear-tools/tabspaces")
+  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  (tabspaces-initialize-project-with-todo t)
+  (tabspaces-todo-file-name "project-todo.org")
+  ;; sessions
+  (tabspaces-session t)
+  (tabspaces-session-auto-restore t)
+  (tab-bar-new-tab-choice "*scratch*"))
